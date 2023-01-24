054ea28d71c060d006769fefcbf17aafc6541000
Teepee78 <eniolatoluwalase78@gmail.com>
2023-01-24 23:48:37 +0100

    Update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   const p1 = uploadPhoto();
   const p2 = createUser();
 
-  Promise.all([p1, p2])
+  return Promise.all([p1, p2])
     .then((data) => {
       console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
     })
