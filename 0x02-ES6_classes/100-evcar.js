bd184eca66d4ba928920e3725dc8178e45d32888
Teepee78 <eniolatoluwalase78@gmail.com>
2023-01-26 13:17:36 +0100

    Update

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -7,7 +7,8 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
-  static cloneCar() {
+  /* eslint-disable class-methods-use-this */
+  cloneCar() {
     return new Car();
   }
 }
