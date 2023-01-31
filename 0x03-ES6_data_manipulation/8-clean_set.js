269fdf36a1ac061296c2dfbe76f9f03202418492
Teepee78 <eniolatoluwalase78@gmail.com>
2023-01-31 09:22:44 +0100

    Update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,15 @@
 export default function cleanSet(set, startString) {
-  if (startString === '' || startString === undefined) return '';
+  if (startString.length === 0 || startString === undefined) return '';
 
-  return [...set].filter((item) => item.startsWith(startString))
+  // Turn set to array
+  let array = [...set];
+  // Filter array
+  array = array.filter((item) => item.startsWith(startString));
+
+  return array
     .reduce(
       (total, item) => {
+        console.log(total);
         if (total !== '') {
           return `${total}-${item.replace(startString, '')}`;
         }
